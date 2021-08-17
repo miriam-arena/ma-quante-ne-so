b858cd2b27de25e4a80c425e0e6faeb247485d0c
Ale <alessandro.gallo.a@gmail.com>
2021-08-17 19:24:39 +0200

    Handling user registration and logged-in cookie
    handle user registration and store a cookie to keep him/her logged in.

---
+++ js/lib.js
@@ -0,0 +1,25 @@
+function getCookie(cname)
+{
+  let name = cname + "=";
+  let decodedCookie = decodeURIComponent(document.cookie);
+  let ca = decodedCookie.split(';');
+  for(let i = 0; i <ca.length; i++) {
+    let c = ca[i];
+    while (c.charAt(0) == ' ') {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return "";
+}
+
+
+
+/* delete a cookie
+
+
+document.cookie = "loginToken=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+
+*/
