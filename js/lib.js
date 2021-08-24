f9782303c028d5aeeb78f45964e95cabce3e1faa
Ale <alessandro.gallo.a@gmail.com>
2021-08-24 19:55:07 +0200

    Storing questions/answer data and more log-in control code
    Now every time a user replies the answer gets saved and reloaded when he goes back to that question.
    Started more work on login control to redirect properly (to the last replied question if necessary).

--- js/lib.js
+++ js/lib.js
@@ -15,11 +15,28 @@ function getCookie(cname)
   return "";
 }
 
-
-
-/* delete a cookie
-
-
-document.cookie = "loginToken=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
-
+/* delete a cookie - as reference
+  document.cookie = "loginToken=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
 */
+
+/* user logged in or already started the quiz or unknown */
+function evaluateUser(data)
+{
+    data = JSON.parse(data);
+
+    if (data.unknown)
+    {
+      data.forward = true;
+      data.location = "log-in.html";
+    }
+    // TODO: finire implementazione per altri casi in cui devo mandare all'ultima risposta data o non devo rimandare da nessuna parte.
+    // TODO: e poi usare questo pattern in tutte le pagine per il controllo del login. [come usato in section.html]
+    /*
+    $.post( "https://artathlon.arternative-lab.eu/api/quizVisitorLogged",{c : getCookie('loginToken'), su : 2}, function( data )
+    {
+      const nextLocation = evaluateUser(data)
+      if (nextLocation.forward)
+         window.location = nextLocation.location;
+    });
+    */
+}
